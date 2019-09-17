753df6ebf3b5cefba7eaf28a21d0d9be693a11b7
sanchows <sanyasach@mail.ru>
2019-09-17 22:20:41 +0300

    getting a lesson  schedule for a few days

--- parser.py
+++ parser.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
+import sys
 
 def get_rasp_by_group(group, nd):
     url = "http://rasp.barsu.by/stud.php"
@@ -11,6 +12,7 @@ def get_rasp_by_group(group, nd):
     )
 
     html = requests.post(url, data = payload).text
+
 
     bs_rasp = BeautifulSoup(html, 'lxml').find('table', class_ = 'text table-bordered').find('tbody')
 
@@ -45,47 +47,49 @@ def get_days(rasp):
 
     return days
 
-def get_pairs_by_day(group, nd, day_number):
+def get_pairs_by_day(group, nd, day_numbers):
     """Возвращает список (bs4) пар на день"""
     rasp_html = get_rasp_by_group(group, nd)
     rasp = get_week_rasp(rasp_html)
 
     rasp_day = {}
 
-    day_name, date = rasp[day_number][0].find_all('td')[:2]
-
     rasp_day['info'] = {
                         'group': group,
                         'week': nd,
                         }
+    for day_number in day_numbers:
+        day_name, date = rasp[day_number][0].find_all('td')[:2]
 
-    rasp_day[f'day_{day_number}'] = {}
-    
-    rasp_day[f'day_{day_number}']['info'] = {
-                                            'day_name': day_name.text.strip(), 
-                                            'date': date.text.strip()
-                                            }
-
-    rasp_day[f'day_{day_number}']['pairs'] = {}
-
-    headers = ('Время занятия', 'Дисциплина', 'Подгр.', 'Преподаватель', 'Аудитория')
-    
-    for pair in range(0, 8):
-        html_by_day = rasp[day_number][pair]
-        columns = html_by_day.find_all('td')[-5:]
+        rasp_day[f'day_{day_number}'] = {}
         
-        rasp_day[f'day_{day_number}']['pairs'][f'pair_{pair+1}'] = dict(zip(headers, columns))
+        rasp_day[f'day_{day_number}']['info'] = {
+                                                'day_name': day_name.text.strip(), 
+                                                'date': date.text.strip()
+                                                }
+
+        rasp_day[f'day_{day_number}']['pairs'] = {}
+
+        headers = ('Время занятия', 'Дисциплина', 'Подгр.', 'Преподаватель', 'Аудитория')
+        
+        for pair in range(0, 8):
+            html_by_day = rasp[day_number][pair]
+            columns = html_by_day.find_all('td')[-5:]
+            
+            rasp_day[f'day_{day_number}']['pairs'][f'pair_{pair+1}'] = dict(zip(headers, columns))
 
     return rasp_day
 
-def parse_pairs_by_day(group, nd, day_number):
-    rasp_day = get_pairs_by_day(group, nd, day_number)
-    pairs = rasp_day.get(f'day_{day_number}').get('pairs')
-    
-    for pair in pairs.values():
-        for key, value in pair.items():
-            tmp_value = str(value).split('<br/>')
-            tmp_value = list(map(lambda x: x.replace('<td>', '').replace('</td>', '').strip(), tmp_value))
-            pair[key] = list(filter(None, tmp_value))
+def parse_pairs_by_day(group, nd, day_numbers):
+    rasp_day = get_pairs_by_day(group, nd, day_numbers)
+
+    for day_number in day_numbers:
+        pairs = rasp_day.get(f'day_{day_number}').get('pairs')
+        
+        for pair in pairs.values():
+            for key, value in pair.items():
+                tmp_value = str(value).split('<br/>')
+                tmp_value = list(map(lambda x: x.replace('<td>', '').replace('</td>', '').strip(), tmp_value))
+                pair[key] = list(filter(None, tmp_value))
 
     return rasp_day
