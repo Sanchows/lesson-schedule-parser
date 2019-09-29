1fbad31d82d9aab5a4657e845cc610b8f9bd2cb4
Sanchows <35726682+Sanchows@users.noreply.github.com>
2019-09-29 17:27:03 +0300

    Update example.py

--- example.py
+++ example.py
@@ -1,5 +1,4 @@
 from parser import *
 
-schd = get_schedule('ИСТ41', nd = '2019-09-23', day_numbers = (0,1,2,))
-
-print(schd)
+for day in get_schedule('ИСТ41', nd = '2019-09-23', day_numbers = (0,1,2,))
+    print(day)
